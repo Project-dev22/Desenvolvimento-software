fcf5c0e53dd33d373aac3377e986235823fd18ef
DanielSantos <kamirates@gmail.com>
2022-10-27 10:02:32 -0300

    ok

--- web-nexts/.next/static/webpack/webpack.02131b4dba5be1f1.hot-update.js
+++ web-nexts/.next/static/webpack/webpack.02131b4dba5be1f1.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fca96ae59a77ab2f"; }
+/******/ 	__webpack_require__.h = function() { return "a98696d22f3f4f69"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
